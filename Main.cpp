6a0a11995b557d78bed5947a5644eb335b0c31ba
rerdtsieck <rich@erdtsieck.com>
2015-12-04 00:15:11 -0800

    Summary
    Desc

--- Main.cpp
+++ Main.cpp
@@ -7,13 +7,15 @@ using namespace std;
 
 int getRow(string);
 int getCol(string);
-void findPush(vector<string>, int);
+int getCol(char);
+vector<string> findPush(vector<string>, int);
+
 
 int getCol(string code)
 {
 	if (code == "program")
 		return 0;
-	if (code == "a")
+	/*if (code == "a")
 		return 1;
 	if (code == "b")
 		return 2;
@@ -48,12 +50,12 @@ int getCol(string code)
 	if (code == "*")
 		return 17;
 	if (code == "/")
-		return 18;
+		return 18;*/
 	if (code == "integer")
 		return 19;
 	if (code == "write")
 		return 20;
-	if (code == "\"")
+	/*if (code == "\"")
 		return 21;
 	if (code == "(")
 		return 22;
@@ -68,11 +70,70 @@ int getCol(string code)
 	if (code == "=")
 		return 27;
 	if (code == "$")
-		return 28;
+		return 28;*/
 	if (code == "begin")
 		return 29;
 	if (code == "end.")
 		return 30;
+
+	cout << endl << "Failed to find Column" << endl;
+	system("pause");
+}
+
+int getCol(char code)
+{
+	if (code == 'a')
+		return 1;
+	if (code == 'b')
+		return 2;
+	if (code == 'c')
+		return 3;
+	if (code == 'd')
+		return 4;
+	if (code == '0')
+		return 5;
+	if (code == '1')
+		return 6;
+	if (code == '2')
+		return 7;
+	if (code == '3')
+		return 8;
+	if (code == '4')
+		return 9;
+	if (code == '5')
+		return 10;
+	if (code == '6')
+		return 11;
+	if (code == '7')
+		return 12;
+	if (code == '8')
+		return 13;
+	if (code == '9')
+		return 14;
+	if (code == '+')
+		return 15;
+	if (code == '-')
+		return 16;
+	if (code == '*')
+		return 17;
+	if (code == '/')
+		return 18;
+	if (code == '"')
+		return 21;
+	if (code == '(')
+		return 22;
+	if (code == ')')
+		return 23;
+	if (code == ',')
+		return 24;
+	if (code == ';')
+		return 25;
+	if (code == ':')
+		return 26;
+	if (code == '=')
+		return 27;
+	if (code == '$')
+		return 28;
 
 	cout << endl << "Failed to find Column" << endl;
 	system("pause");
@@ -83,19 +144,19 @@ int getRow(string poppedStack)
 		return 0;
 	if (poppedStack == "identifier")
 		return 1;
-	if (poppedStack == "a")
+	if (poppedStack == "A")
 		return 2;
 	if (poppedStack == "dec-list")
 		return 3;
 	if (poppedStack == "dec")
 		return 4;
-	if (poppedStack == "c")
+	if (poppedStack == "C")
 		return 5;
 	if (poppedStack == "type")
 		return 6;
 	if (poppedStack == "stat-list")
 		return 7;
-	if (poppedStack == "d")
+	if (poppedStack == "D")
 		return 8;
 	if (poppedStack == "stat")
 		return 9;
@@ -107,17 +168,17 @@ int getRow(string poppedStack)
 		return 12;
 	if (poppedStack == "expr")
 		return 13;
-	if (poppedStack == "e")
+	if (poppedStack == "E")
 		return 14;
 	if (poppedStack == "term")
 		return 15;
-	if (poppedStack == "q")
+	if (poppedStack == "Q")
 		return 16;
 	if (poppedStack == "factor")
 		return 17;
 	if (poppedStack == "number")
 		return 18;
-	if (poppedStack == "b")
+	if (poppedStack == "B")
 		return 19;
 	if (poppedStack == "sign")
 		return 20;
@@ -128,7 +189,7 @@ int getRow(string poppedStack)
 	cout << endl << "Failed to find Row" << endl;
 	system("pause");
 }
-void findPush(vector<string> stack, int grammar)
+vector<string> findPush(vector<string> stack, int grammar)
 {
 	if (grammar == 1)
 	{
@@ -140,21 +201,25 @@ void findPush(vector<string> stack, int grammar)
 		stack.push_back(";");
 		stack.push_back("identifier");
 		stack.push_back("program");
+		return stack;
 	}
 	if (grammar == 2)
 	{
+		stack.push_back("A");
 		stack.push_back("id");
-		stack.push_back("A");;
+		return stack;
 	}
 	if (grammar == 3)
 	{
 		stack.push_back("A");
-		stack.push_back("id");;
+		stack.push_back("id");
+		return stack;
 	}
 	if (grammar == 4)
 	{
 		stack.push_back("A");
 		stack.push_back("digit");
+		return stack;
 	}
 	if (grammar == 6)
 	{
@@ -162,37 +227,45 @@ void findPush(vector<string> stack, int grammar)
 		stack.push_back("type");
 		stack.push_back(":");
 		stack.push_back("dec");
+		return stack;
 	}
 	if (grammar == 7)
 	{
 		stack.push_back("C");
 		stack.push_back("identifier");
+		return stack;
 	}
 	if (grammar == 8)
 	{
 		stack.push_back("dec");
 		stack.push_back(",");
+		return stack;
 	}
 	if (grammar == 10)
 	{
 		stack.push_back("integer");
+		return stack;
 	}
 	if (grammar == 11)
 	{
 		stack.push_back("D");
 		stack.push_back("stat");
+		return stack;
 	}
 	if (grammar == 12)
 	{
 		stack.push_back("write");
+		return stack;
 	}
 	if (grammar == 14)
 	{
 		stack.push_back("write");
+		return stack;
 	}
 	if (grammar == 15)
 	{
 		stack.push_back("assign");
+		return stack;
 	}
 	if (grammar == 16)
 	{
@@ -202,11 +275,13 @@ void findPush(vector<string> stack, int grammar)
 		stack.push_back("str");
 		stack.push_back("(");
 		stack.push_back("write");
+		return stack;
 	}
 	if (grammar == 17)
 	{
 		stack.push_back("\"");
 		stack.push_back("\"");
+		return stack;
 	}
 	if (grammar == 19)
 	{
@@ -214,27 +289,166 @@ void findPush(vector<string> stack, int grammar)
 		stack.push_back("expr");
 		stack.push_back("=");
 		stack.push_back("identifier");
+		return stack;
 	}
 	if (grammar == 20)
 	{
 		stack.push_back("E");
 		stack.push_back("term");
+		return stack;
 	}
 	if (grammar == 21)
 	{
 		stack.push_back("expr");
 		stack.push_back("+");
+		return stack;
 	}
 	if (grammar == 22)
 	{
 		stack.push_back("expr");
 		stack.push_back("-");
+		return stack;
+	}
+	if (grammar == 24)
+	{
+		stack.push_back("Q");
+		stack.push_back("factor");
+		return stack;
+	}
+	if (grammar == 25)
+	{
+		stack.push_back("Q");
+		stack.push_back("factor");
+		stack.push_back("*");
+		return stack;
+	}
+	if (grammar == 26)
+	{
+		stack.push_back("Q");
+		stack.push_back("factor");
+		stack.push_back("/");
+		return stack;
+	}
+	if (grammar == 28)
+	{
+		stack.push_back("identifier");
+		return stack;
+	}
+	if (grammar == 29)
+	{
+		stack.push_back("number");
+		return stack;
+	}
+	if (grammar == 30)
+	{
+		stack.push_back("(");
+		stack.push_back("expr");
+		stack.push_back(")");
+		return stack;
+	}
+	if (grammar == 31)
+	{
+		stack.push_back("B");
+		stack.push_back("digit");
+		stack.push_back("sign");
+		return stack;
+	}
+	if (grammar == 32)
+	{
+		stack.push_back("B");
+		stack.push_back("digit");
+		return stack;
+	}
+	if (grammar == 38)
+	{
+		stack.push_back("0");
+		return stack;
+	}
+	if (grammar == 39)
+	{
+		stack.push_back("1");
+		return stack;
+	}
+	if (grammar == 40)
+	{
+		stack.push_back("2");
+		return stack;
+	}
+	if (grammar == 41)
+	{
+		stack.push_back("3");
+		return stack;
+	}
+	if (grammar == 42)
+	{
+		stack.push_back("4");
+		return stack;
+	}
+	if (grammar == 43)
+	{
+		stack.push_back("5");
+		return stack;
+	}
+	if (grammar == 44)
+	{
+		stack.push_back("6");
+		return stack;
+	}
+	if (grammar == 45)
+	{
+		stack.push_back("7");
+		return stack;
+	}
+	if (grammar == 46)
+	{
+		stack.push_back("8");
+		return stack;
+	}
+	if (grammar == 47)
+	{
+		stack.push_back("9");
+		return stack;
+	}
+	if (grammar == 48)
+	{
+		stack.push_back("a");
+		return stack;
+	}
+	if (grammar == 49)
+	{
+		stack.push_back("b");
+		return stack;
+	}
+	if (grammar == 50)
+	{
+		stack.push_back("c");
+		return stack;
+	}
+	if (grammar == 51)
+	{
+		stack.push_back("d");
+		return stack;
+	}
+	if (grammar == 52)
+	{
+		stack.push_back("integer");
+		return stack;
+	}
+	if (grammar == 53)
+	{
+		stack.push_back("+");
+		return stack;
+	}
+	if (grammar == 54)
+	{
+		stack.push_back("-");
+		return stack;
 	}
 }
 
 
 int main()
-{	// 0 = blank; 37 = lambda; 38-47 = 0-9; 48-51 = a-d; 52 = integer; + = 52; - = 53 0 = blank
+{	// 0 = blank; 37 = lambda; 38-47 = 0-9; 48-51 = a-d; 52 = integer; 53 = +; 54 = -; 0 = blank
 	//			0		1	2	3	4	5	6	7	8	9	10	11	12	13	14	15	16	17	18	19		20		21	22	23	24	25	26	27	28	29		30
 	/*			Program	a	b	c	d	0	1	2	3	4	5	6	7	8	9	+	-	*	/	integer	write	"	(	)	,	;	:	=	$	begin	end
 	Prog		1
@@ -312,6 +526,9 @@ int main()
 	inputFile >> currentSyntax;
 
 	cout << currentSyntax << endl;
+
+	int counter = 0;
+
 	while (!stack.empty() && isValid == true)
 	{
 
@@ -322,7 +539,7 @@ int main()
 			cout << "Current Stack: ";
 			for (vector<string>::iterator it = stack.begin(); it != stack.end(); ++it)
 			{
-				cout << *it;
+				cout << *it << " ";
 			}
 
 			cout << endl << "Matched: " << currentSyntax;
@@ -336,115 +553,53 @@ int main()
 		}
 		else
 		{
-			col = getCol(currentSyntax);
+			if (currentSyntax == "program" || currentSyntax == "integer" || currentSyntax == "write" || currentSyntax == "begin" || currentSyntax == "end.")
+			{
+				col = getCol(currentSyntax);
+			}
+			else
+			{
+				col = getCol(currentSyntax[counter]);
+				counter++;
+			}
+
 			row = getRow(stack.back());
-			/*switch (word[wordCounter])
-			{
-			case 'i':
-			{
-				col = 0;
-				break;
-			}
-			case '+':
-			{
-				col = 1;
-				break;
-			}
-			case '-':
-			{
-				col = 2;
-				break;
-			}
-			case '*':
-			{
-				col = 3;
-				break;
-			}
-			case '/':
-			{
-				col = 4;
-				break;
-			}
-			case '(':
-			{
-				col = 5;
-				break;
-			}
-			case ')':
-			{
-				col = 6;
-				break;
-			}
-			case '$':
-			{
-				col = 7;
-				break;
-			}
-
-			}//END SWITCH*/
-
-			/*switch (stack.back())
-			{
-			case 'E':
-			{
-				row = 0;
-				break;
-			}
-			case 'Q':
-			{
-				row = 1;
-				break;
-			}
-			case 'T':
-			{
-				row = 2;
-				break;
-			}
-			case 'R':
-			{
-				row = 3;
-				break;
-			}
-			case 'F':
-			{
-				row = 4;
-				break;
-			}
-			}//END SWITCH*/
 
 			stack.pop_back();
 
 			if (grammarTable[row][col] != 0 && grammarTable[row][col] != 37)
 			{
+				stack = findPush(stack, grammarTable[row][col]);
+			}
+			cout << "Current Stack" << endl;
+			for (vector<string>::iterator it = stack.begin(); it != stack.end(); ++it)
+			{
+				cout << *it << " ";
+			}
+			cout << endl << endl;
+
+			/*if (grammarTable[row][col] != 0 && grammarTable[row][col] != 37)
+			{
 				for (int::reverse_iterator rit = grammarTable[row][col].rbegin(); rit != grammarTable[row][col].rend(); ++rit)
 				{
 					stack.push_back(*rit);
 				}
-			}
+			}*/
 
 			if (grammarTable[row][col] == 0)
 			{
 				isValid = false;
 			}
-
-
 		}
-
-		/*cout << "Current Stack: ";
-		for (vector<char>::iterator it = stack.begin(); it != stack.end(); ++it)
-		{
-		cout << *it;
-		}
-		cout << endl << "Looking For: " << word[wordCounter] << endl << endl;
-	}//END WHILE*/
+	}//END WHILE
 
 	if (isValid)
 	{
-		cout << word << " is Valid." << endl;
+		cout << "Program is Valid." << endl;
 	}
 	else
 	{
-		cout << word << " is not valid." << endl;
+		cout << "Program is not valid." << endl;
 	}
 	
 	system("pause");
